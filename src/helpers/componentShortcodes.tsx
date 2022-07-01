0e402870b9711002fb7f1870e8ca15d3f4e0b019
Liana Pigeot <32243840+nialna@users.noreply.github.com>
2022-07-02 00:23:08 +0100

    more changes

--- src/helpers/componentShortcodes.tsx
+++ src/helpers/componentShortcodes.tsx
@@ -1,6 +1,8 @@
 import { ImagesGrid } from "../components/image-grid/image-grid";
 import { NewsList } from "../components/news-list";
+import { Badge } from "theme-ui";
 export const shortcodes = {
   ImagesGrid,
   NewsList,
+  Badge,
 };
